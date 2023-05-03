501ba28066553711008a87e28b71e9a53873d493
shamal <shamal.bherde@neosoftmail.com>
2023-05-03 18:10:36 +0530

    first commit

--- Frontend/react-frontend/src/components/wallet/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MetaContext } from "./MetaContext";
-export { default as MetaProvider } from "./MetaProvider";
-export { default as useMeta } from "./useMeta";
-export * from "./state";
--- Frontend/react-frontend/src/components/wallet/useMeta.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import MetaContext from "./MetaContext";
-
-const useMeta = () => useContext(MetaContext);
-
-export default useMeta;
